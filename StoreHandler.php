6564eeffb12aa3facc457e6bbb4a3ae9c4fc8646
olexandr <alexx1984@ukr.net>
2024-01-03 14:21:04 +0200

    add simple validation

--- StoreHandler.php
+++ StoreHandler.php
@@ -7,29 +7,20 @@ class StoreHandler
     private mixed $store;
     private mixed $passwordEncryptor;
 
-    public function __construct($storeClass,  $passwordEncryptorClass)
+    public function __construct($storeClass, $passwordEncryptorClass)
     {
         $this->store = new $storeClass();
         $this->passwordEncryptor = new $passwordEncryptorClass(ENCRYPTION_KEY); // Ensure ENCRYPTION_KEY is defined
     }
 
-    public function showMessage(string $message): void
-    {
-        echo $message . "\n";
-    }
-
-
     public function inputActions(): string
     {
         return readline("Enter your choice action: ");
     }
-
     public function inputPassword(): void
     {
-        $passwordName = trim(readline("Enter password name: "));
-        $this->showEmptyFieldError('password name',$passwordName);
-        $passwordValue = trim(readline("Enter password value: "));
-        $this->showEmptyFieldError('password value',$passwordValue);
+        $passwordName = $this->getField('password name');
+        $passwordValue = $this->getField('password value');
 
         $encryptedPassword = $this->passwordEncryptor->encryptPassword($passwordValue);
         $this->store->setPassword($passwordName, $encryptedPassword);
@@ -40,15 +31,27 @@ class StoreHandler
         $this->store->showAllPasswords();
     }
 
+    private function getField($fieldName)
+    {
+        $fieldValue = trim(readline("Enter $fieldName: "));
+        if ($fieldValue === '') {
+            echo "$fieldName can't be empty.\n";
+
+            return $this->getField($fieldName);
+        } else {
+            return $fieldValue;
+        }
+    }
+
     public function getPassword(): void
     {
-        $passwordName = trim(readline("Enter password name: "));
-        $this->showEmptyFieldError('password name',$passwordName);
+        $passwordName = $this->getField('password name');
 
         $passwordValue = $this->store->getPassword($passwordName);
         if ($passwordValue !== null) {
             $decryptedPassword = $this->passwordEncryptor->decryptPassword($passwordValue);
-            echo "Password value: " . $decryptedPassword . "\n";
+
+            echo "Password: $decryptedPassword\n";
         } else {
             echo "Password not found.\n";
         }
@@ -56,26 +59,44 @@ class StoreHandler
 
     public function deletePassword(): void
     {
-        $passwordName = readline("Enter password name: ");
-        $this->showEmptyFieldError('password name',$passwordName);
-        $this->store->deletePassword($passwordName);
+        $passwordName = $this->getField('password name');
+        $passwordValue = $this->store->getPassword($passwordName);
+
+        if ($passwordValue !== null) {
+            $this->store->deletePassword($passwordName);
+            echo "Password deleted.\n";
+        } else {
+            echo "Password not found.\n";
+        }
     }
 
     public function replacePassword(): void
     {
-        $passwordName = trim(readline("Enter password name: "));
-        $this->showEmptyFieldError('password name',$passwordName);
-        $passwordValue = trim(readline("Enter password value: "));
-        $this->showEmptyFieldError('password value',$passwordValue);
+        $passwordName = $this->getField('password name');
+        $passwordValue = $this->getField('password value');
+        $oldPasswordValue = $this->store->getPassword($passwordName);
 
+        if ($oldPasswordValue === null) {
+            echo "Password not found.\n";
+            return;
+        } else {
+            $decryptedPassword = $this->passwordEncryptor->decryptPassword($oldPasswordValue);
+
+            if ($decryptedPassword === $passwordValue) {
+                echo "New password can't be the same as old password.\n";
+                return;
+            }
+        }
         $encryptedPassword = $this->passwordEncryptor->encryptPassword($passwordValue);
         $this->store->replacePassword($passwordName, $encryptedPassword);
+
+        echo "Password replaced.\n";
     }
 
     private function showEmptyFieldError($fieldName, $fieldValue): void
     {
-        if($fieldValue === '') {
-            $this->showMessage("$fieldName can't be empty.");
+        if ($fieldValue === '') {
+            echo "$fieldName can't be empty.\n";
             return;
         }
     }
