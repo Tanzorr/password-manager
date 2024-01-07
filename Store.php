af81c436a4cefc8b8d5a94ffb8e9c279588447fd
olexandr <alexx1984@ukr.net>
2024-01-07 22:53:43 +0200

    add readPaswordFile method

--- Store.php
+++ Store.php
@@ -3,18 +3,21 @@
 class Store
 {
     private string $passwordsFile = 'passwords.json';
-
-
-    public function getPasswords(): array
+    private function readPasswordsFile(): array
     {
-        $passwords = file_get_contents($this->passwordsFile);
+        $passwords = @file_get_contents($this->passwordsFile);
+
+        if ($passwords === false) {
+            return [];
+        }
 
         return json_decode($passwords, true) ?? [];
     }
 
+
     public function setPassword($passwordName, $passwordValue): void
     {
-        $passwords = $this->getPasswords();
+        $passwords = $this->readPasswordsFile();
         $passwords[$passwordName] = $passwordValue;
         $passwords = json_encode($passwords);
         file_put_contents($this->passwordsFile, $passwords);
@@ -22,13 +25,13 @@ class Store
 
     public function getPassword($passwordName)
     {
-        $passwords = $this->getPasswords();
+        $passwords = $this->readPasswordsFile();
         return $passwords[$passwordName];
     }
 
     public function deletePassword($passwordName): void
     {
-        $passwords = $this->getPasswords();
+        $passwords = $this->readPasswordsFile();
         unset($passwords[$passwordName]);
         $passwords = json_encode($passwords);
         file_put_contents($this->passwordsFile, $passwords);
@@ -36,7 +39,7 @@ class Store
 
     public function showAllPasswords(): void
     {
-        $passwords = $this->getPasswords();
+        $passwords = $this->readPasswordsFile();
         if(count($passwords) === 0) {
             echo "No passwords found.\n";
             return;
@@ -49,7 +52,7 @@ class Store
 
     public function replacePassword($passwordName, $passwordValue): void
     {
-        $passwords = $this->getPasswords();
+        $passwords = $this->readPasswordsFile();
         $passwords[$passwordName] = $passwordValue;
         $passwords = json_encode($passwords);
         file_put_contents($this->passwordsFile, $passwords);
